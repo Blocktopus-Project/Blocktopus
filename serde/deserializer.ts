78f39dd1eff82596cfebe5476867102694eb4df8
MierenManz <christiaanmens@gmail.com>
2022-03-15 22:30:55 +0100

    x

--- serde/deserializer.ts
+++ serde/deserializer.ts
@@ -51,6 +51,74 @@ function deserializePlayPackets(
       };
     }
 
+    case 0x03:
+      return {
+        state: State.Play,
+        packedID,
+        message: deserializeString(buffer)[0],
+      };
+
+    case 0x04:
+      return {
+        state: State.Play,
+        packedID,
+        actionID: readVarInt(buffer)[0],
+      };
+
+    case 0x05: {
+      let bytesRead = 0;
+      const localeString = deserializeString(buffer);
+      bytesRead += localeString[1];
+
+      const viewDistance = buffer.subarray(bytesRead)[0];
+      bytesRead++;
+
+      const chatMode = readVarInt(buffer.subarray(bytesRead));
+      bytesRead += chatMode[1];
+
+      const [chatColor, skinFlagsByte] = buffer.subarray(bytesRead);
+      bytesRead += 2;
+
+      const mainHand = readVarInt(buffer.subarray(bytesRead));
+      bytesRead += mainHand[1];
+
+      const misc = buffer.subarray(
+        bytesRead,
+      );
+
+      return {
+        state: State.Play,
+        packedID,
+        locale: localeString[0],
+        viewDistance,
+        chatMode: chatMode[0],
+        chatColors: !!chatColor,
+        displayedSkinParts: {
+          cape: !!(skinFlagsByte & 0x01),
+          jacket: !!(skinFlagsByte & 0x02),
+          leftSleeve: !!(skinFlagsByte & 0x04),
+          rightSleeve: !!(skinFlagsByte & 0x08),
+          leftPantsLeg: !!(skinFlagsByte & 0x10),
+          rightPantsLeg: !!(skinFlagsByte & 0x20),
+          hat: !!(skinFlagsByte & 0x40),
+        },
+        mainHand: mainHand[0],
+        enableTextFiltering: !!misc[0],
+        allowServerListings: !!misc[1],
+      };
+    }
+
+    case 0x06: {
+      const [transactionID, readBytes] = readVarInt(buffer);
+      return {
+        state: State.Play,
+        packedID,
+        transactionID: transactionID,
+        text: deserializeString(buffer.subarray(readBytes))[0],
+      };
+    }
+
+    case 0x02:
     default:
       throw new Error("Invalid or Unsupported Packet");
   }
@@ -61,13 +129,13 @@ function deserializeLoginPackets(
   packedID: number,
 ): LoginPayloads {
   switch (packedID) {
-    case 0x00: {
+    case 0x00:
       return {
         state: State.Login,
         packedID,
         name: deserializeString(buffer)[0],
       };
-    }
+
     default:
       throw new Error("Invalid or Unsupported Packet");
   }
